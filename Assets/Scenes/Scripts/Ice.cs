0feb0ae8fa43d93446075fafa0c4c9469a02e5d8
Darianlime <darianli447@gmail.com>
2025-04-28 18:23:11 -0500

    Water Falling Animation

--- Assets/Scenes/Scripts/Ice.cs
+++ Assets/Scenes/Scripts/Ice.cs
@@ -17,6 +17,9 @@ public class Ice : MonoBehaviour
     public SkinnedMeshRenderer meshRendererOutside;
     public SkinnedMeshRenderer meshRendererInside;
     private float startTime = 0f;
+    private Ray ray;
+    public float maxRayDistance = 0.5f;
+    private int layerMask;
     private void Awake()
     {
         AnimationActions.current.IceGrow += GrowIce;
@@ -29,6 +32,30 @@ public class Ice : MonoBehaviour
 
     private void Start() {
         boxCollider = GetComponent<BoxCollider>();
+        layerMask = 1 << LayerMask.NameToLayer("Roof");
+    }
+
+    public void Update()
+    {
+        RayCastFloor();
+        Debug.DrawLine(this.transform.position + new Vector3(0, 1, 0), Vector3.down*maxRayDistance + this.transform.position + new Vector3(0, 1, 0), Color.red);  
+    }
+
+    public void RayCastFloor() {
+        ray = new Ray(this.transform.position + new Vector3(0, 1, 0), Vector3.down);
+        if (Physics.Raycast(ray, out RaycastHit hit, maxRayDistance, layerMask)) {
+            if (hit.collider != null) {
+                animatorOutside.SetBool("isFallingDone", true);
+                if (animatorOutside.GetCurrentAnimatorStateInfo(0).IsName("IceToWaterOutside")) {
+                    WaterCollider();
+                }
+            } 
+        } else {
+            animatorOutside.SetBool("isFallingDone", false);
+            if (animatorOutside.GetCurrentAnimatorStateInfo(0).IsName("IceToWaterFalling")) {
+                IceCollider();
+            }
+        }
     }
 
     public void IceCollider() {
@@ -42,38 +69,36 @@ public class Ice : MonoBehaviour
     }
 
     public void GrowIce() {
+        //animatorOutside.SetBool("isFallingDone", false);
         animatorOutside.CrossFade("IceGrowOutside", 0.5f, 0);
         animatorInside.CrossFade("IceGrowInside", 0.5f, 0);
     }
 
     public void DissolveIce() {
-        animatorInside.SetBool("GrowingContinuing", false);
-        animatorOutside.SetBool("GrowingContinuing", false);
+        //animatorOutside.SetBool("isFallingDone", false);
         animatorOutside.CrossFade("IceDissolveOutside", 0.5f, 0);
         animatorInside.CrossFade("IceDissolveInside", 0.5f, 0);
     }
 
     public void IceToWater() {
-        animatorInside.SetBool("GrowingContinuing", false);
-        animatorOutside.SetBool("GrowingContinuing", false);
-        if (animatorInside.GetCurrentAnimatorStateInfo(0).IsName("IceDissolveInside")) {
-            animatorInside.CrossFade("IceGrowInsideHelper", 0.2f, 0);
-            animatorOutside.CrossFade("IceGrowOutsideHelper", 0.2f, 0);
-        } else {
+        if (!animatorOutside.GetBool("isFallingDone")) {
+            //animatorOutside.SetBool("isFallingDone", false);
+            animatorInside.CrossFade("IceDissolveInsideRev", 0.2f, 0);
+            animatorOutside.CrossFade("IceToWaterFalling", 0.2f, 0);
+        } else if (animatorOutside.GetBool("isFallingDone")) {
             animatorInside.CrossFade("IceToWaterInside", 0.2f, 0);
             animatorOutside.CrossFade("IceToWaterOutside", 0.2f, 0);
         }
     }
 
     public void WaterToIce() {
-        if (!animatorInside.GetCurrentAnimatorStateInfo(0).IsName("IceToWaterInside")) {
-            startTime = animatorInside.GetCurrentAnimatorStateInfo(0).normalizedTime;
-            animatorInside.CrossFade("IceGrowInsideHelper", 0.2f, 0, startTime);
-            animatorOutside.CrossFade("IceGrowOutsideHelper", 0.2f, 0, startTime);
-            animatorInside.SetBool("GrowingContinuing", true);
-            animatorOutside.SetBool("GrowingContinuing", true);
+        //animatorOutside.SetBool("isFallingDone", false);
+        if (animatorOutside.GetCurrentAnimatorStateInfo(0).IsName("IceToWaterFalling")) {
+            startTime = animatorOutside.GetCurrentAnimatorStateInfo(0).normalizedTime;
+            animatorInside.CrossFade("IceGrowInsideRev", 0.2f, 0, 0);
+            animatorOutside.CrossFade("WaterToIceFalling", 0.2f, 0, 0);
         } else {
-            animatorInside.CrossFade("WaterToIceInside", 0.2f, 0);
+            animatorInside.CrossFade("WaterToIceInside", 0.2f, 0, 0);
             animatorOutside.CrossFade("WaterToIceOutside", 0.2f, 0);
         }
     }
